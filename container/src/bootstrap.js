8b2bf4b94ee25fda53c1c421c2fd571332e6573d
Dejan Hadziomerovic <dejan.hadziomerovic@zalando.de>
2024-01-30 16:20:35 +0100

    export mount function to let container decide how to render products and cart

--- container/src/bootstrap.js
+++ container/src/bootstrap.js
@@ -1,4 +1,8 @@
-import "products/ProductsIndex";
+import { mount as productsMount } from "products/ProductsIndex";
+import { mount as cartMount } from "cart/CartShow";
 import "cart/CartShow";
 
 console.log("Hello from container bootstrap");
+
+productsMount(document.querySelector("#container-products"));
+cartMount(document.querySelector("#container-cart"));
