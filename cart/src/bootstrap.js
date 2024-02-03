c2049e75a90b2185b9d67e678682c765089fdf73
Dejan Hadziomerovic <dejan.hadziomerovic@zalando.de>
2024-02-03 10:51:03 +0100

    changes in webpack configuration

--- cart/src/bootstrap.js
+++ cart/src/bootstrap.js
@@ -1,5 +1,18 @@
 import faker from 'faker';
 
-const cartText = `<div>You have ${faker.random.number()} items in your cart</div>`;
+const mount = (el) => {
+  const cartText = `<div>You have ${faker.random.number()} items in your cart</div>`;
 
-document.querySelector('#cart-dev').innerHTML = cartText;
+  el.innerHTML = cartText;
+}
+
+if (process.env.NODE_ENV === "development") {
+  const el = document.querySelector("#dev-cart");
+
+  // Assuming our container doesn't have an element with id 'dev-products'
+  if (el) {
+    mount(el);
+  }
+}
+
+export { mount };
